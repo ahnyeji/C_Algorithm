2e3779f2ad3f8e78141db0f505b204440a5ad77d
ahnyeji <yejian1218@naver.com>
2021-02-01 00:49:49 +0900

    01.FEB.2021 / implement

---
+++ DataStructure2/11279.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+using namespace std;
+
+
+
+int heap[100001];
+int sizeIdx = 0;
+void insert(int x){
+    heap[++sizeIdx] = x;
+    int child = sizeIdx;
+    while(1){
+        if(child == 1) return;
+        int parent = child / 2;
+        if(heap[child] > heap[parent]){
+            int tmp = heap[child];
+            heap[child] = heap[parent];
+            heap[parent] = tmp;
+            child = parent;
+        }
+        else{
+            return;
+        }
+    }
+}
+
+void remove(){
+    if(sizeIdx == 0){
+        cout << "0\n";
+        return;
+    }
+    cout << heap[1] << "\n";
+    heap[1] = heap[sizeIdx];
+    heap[sizeIdx--] = 0;
+    int parent = 1;
+        while(1){
+            int left = parent * 2;
+            int right = parent * 2 + 1;
+            if(left > sizeIdx) return;
+            int tochange;
+            if(right > sizeIdx){
+                if(heap[parent] < heap[left]){
+                    tochange = left;
+                }
+                else return;
+            }
+            else{
+                if(heap[parent] >= heap[left]){
+                    if(heap[parent] >= heap[right]) return;
+                    else{
+                        tochange = right;
+                    }
+                }
+                else{
+                    if(heap[parent] >= heap[right]){
+                        tochange = left;
+                    }
+                    else{
+                        tochange = heap[left] > heap[right] ? left : right;
+                    }
+                }
+            }
+            int tmp = heap[parent];
+            heap[parent] = heap[tochange];
+            heap[tochange] = tmp;
+            parent = tochange;
+    }
+}
+
+int main(){
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    int N;
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        int x;
+        cin >> x;
+        if(x){
+            insert(x);
+        }
+        else{
+            remove();
+        }
+    }
+}
