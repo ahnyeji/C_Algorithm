4b9ad6f5ed3e8cd0ffc39a0feee102c947449cad
ahnyeji <yejian1218@naver.com>
2021-02-01 00:50:59 +0900

    01.FEB.2021 / using STL

---
+++ DataStructure2/1927STL.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    int N;
+    cin >> N;
+    priority_queue<int, vector<int>, greater<int> > pq;
+    for(int i = 0; i < N; i++){
+        int x;
+        cin >> x;
+        if(x){
+            pq.push(x);
+        }
+        else{
+            if(pq.empty()){
+                cout << "0\n";
+            }
+            else{
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        }
+    }
+}
