ae3648bed22ae573e360337db2ddb16353274911
ahnyeji <yejian1218@naver.com>
2021-03-29 19:52:03 +0900

    29.MAR.2021 / 집합의 표현

--- BOJ/UnionFind/1717.cpp
+++ BOJ/UnionFind/1717.cpp
@@ -1,3 +1,11 @@
+/*  BOJ - 1717 : 집합의 표현
+    29.March.2021 
+ */
+
+/*  [Union Find (Disjoint Set)]
+    - Path Compression : 6320KB / 36ms
+    - Union by Rank : 9828KB / 44ms (rank 저장하기 위한 배열 사용하므로 메모리 더 필요)
+ */
 #include <iostream>
 
 using namespace std;
@@ -22,7 +30,7 @@ int main() {
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
     cin >> n >> m;
-    for(int i = 1; i <= n; i++) { // 대표자 배열 초기화 (자기 자신만 속한 집합)
+    for(int i = 1; i <= n; i++) { // make set : 대표자 배열 초기화 (자기 자신만 속한 집합)
         p[i] = i;
     }
 
@@ -33,3 +41,49 @@ int main() {
         else cout << (find(a) == find(b) ? "YES\n" : "NO\n");
     }
 }
+
+/*
+#include <iostream>
+
+using namespace std;
+
+int p[1000001];
+int r[1000001];
+
+int find(int x) {
+    if(x == p[x]) return x;
+    return find(p[x]);
+}
+
+void merge(int x, int y) { // 최적화; union by rank (rank=깊이 큰 트리에 작은 트리 붙이기)
+    x = find(x);
+    y = find(y);
+    if(x == y) return;
+
+    if(r[x] == r[y]) r[y]++; // 깊이 같으면 둘 중 하나의 깊이 1 증가
+    
+    // 깊이 큰 쪽에 작은 쪽 붙이기
+    if(r[x] > r[y]) p[y] = x;
+    else p[x] = y;
+}
+
+int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    
+    int n, m;
+    cin >> n >> m;
+    
+    for(int i = 1; i <= n; i++) { // make set : 대표자 배열 초기화
+        p[i] = i;
+        r[i] = 1; // 깊이 모두 1로 초기화
+    }
+
+    int op, a, b;
+    for(int i = 0; i < m; i++) {
+        cin >> op >> a >> b;
+        if(op == 0) merge(a, b);
+        else cout << (find(a) == find(b) ? "YES\n" : "NO\n");
+    }
+}
+*/
